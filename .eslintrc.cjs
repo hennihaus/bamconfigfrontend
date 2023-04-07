497f9da52348d561d8942cfff333e07f87458c2f
janhendrik hausner <janhendrik.hausner@tchibo.de>
2023-04-07 17:31:41 +0200

    optimized prettier and eslint #patch

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,25 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier",
-  ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: "latest"
   },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/eslint-config-typescript",
+    "eslint-config-prettier"
+  ],
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/vue"],
+      rules: {
+        "testing-library/prefer-screen-queries": "off",
+        "testing-library/render-result-naming-convention": "off"
+      }
+    }
+  ],
+  noInlineConfig: true,
+  reportUnusedDisableDirectives: true
 };
